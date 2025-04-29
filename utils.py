57168f96762fa8106a475bdfb53f76fd7cdb827a
student app <madhav@gmail.com>
2025-04-29 16:26:53 +0530

    updated

--- utils.py
+++ utils.py
@@ -97,8 +97,13 @@ def download_model(file_id, output_path):
     :param file_id: The Google Drive file ID.
     :param output_path: The local path to save the file.
     """
+    output_dir = os.path.dirname(output_path)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
     if not os.path.exists(output_path):
         print(f"Downloading {output_path}...")
+        if(file_id == "none"):return
         url = f"https://drive.google.com/uc?id={file_id}"
         gdown.download(url, output_path, quiet=False)
     else:
@@ -107,8 +112,8 @@ def download_model(file_id, output_path):
 def download_and_load_models():
     model_files = {
         "retriever_model": {
-            "file_id": "1JA2pgCbPTqqjOtur6wG0p_zgiiI8SCRS",  # Replace with your Google Drive file ID
-            "output_path": "models/120aBRS.pt"
+            "file_id": "1ExGaHlj2RY7FR6V3gOkzQv6N61CjyQY8",  # Replace with your Google Drive file ID
+            "output_path": "models/200aBRS.pt"
         },
         "generator_model": {
             "file_id": "1M0YWjjOfpJ5pEZNxTQEiRYFIvkZu7v6S",  # Replace with your Google Drive file ID
@@ -127,16 +132,64 @@ def download_and_load_models():
             "output_path": "data/test.json"
         },
         "detr_processor": {
-            "file_id": "1XEePJlhpk_smKWfwE3ZJLnlyOiE553vz",  # Replace with your Google Drive file ID
+            "file_id": "none",  # Replace with your Google Drive file ID
             "output_path": "models/detr_processor"
         },
+        "det_en":{
+            "file_id": "1qNjrdQ8geOOIlmbgJ056OY1v3DXuCa7T",  # Replace with your Google Drive file ID
+            "output_path": "models/detr_processor/models--facebook--detr-resnet-50/blobs/414dfcd4653475c1080e92b0b95d81ff649988e6"
+        },
+        "det_refs":{
+            "file_id": "15md964OoxwYNDywVd3R1Ar8ILKTBBqM7",  # Replace with your Google Drive file ID   
+            "output_path": "models/detr_processor/models--facebook--detr-resnet-50/refs/main"
+        },
+        "def_snap":{
+            "file_id": "1KHXeeQZ_DYACg5KlUBQ3igcvI2tsqCGw",  # Replace with your Google Drive file ID
+            "output_path": "models/detr_processor/models--facebook--detr-resnet-50/snapshots/1d5f47bd3bdd2c4bbfa585418ffe6da5028b4c0b/preprocessor_config.json"
+        },
+        "detr_processor_config":{
+            "file_id": "14QmnZjSE66ixenneMQSDWD9Jz0JLNvf9",  # Replace with your Google Drive file ID
+            "output_path": "models/detr_processor/preprocessor_config.json"
+        },
         "detr_model": {
-            "file_id": "1vyFvWoImwhW9ApGAUMtSyHLT7dDDxKOA",  # Replace with your Google Drive file ID
+            "file_id": "none",  # Replace with your Google Drive file ID
             "output_path": "models/detr_model"
         },
+        "detr_model_config":{
+            "file_id": "1WCKP5rVHGRVqSsNOazPBujO63UNu1sJj",  # Replace with your Google Drive file ID
+            "output_path": "models/detr_model/config.json"
+        },
+        "detr_model_checkpoint":{
+            "file_id": "1bVXmFkE8fQVBOzrQIqxRCICPzAWAosks",  # Replace with your Google Drive file ID
+            "output_path": "models/detr_model/model.safetensors"
+        },
         "doc_summary":{
-            "file_id": "19QqLpZymJetTdG2k04JDyXWfoJNeGMGu",  # Replace with your Google Drive file ID
+            "file_id": "none",  # Replace with your Google Drive file ID
             "output_path": "models/doc_summary_model"
+        },
+        "doc_summary_config":{
+            "file_id": "1QnLX3O7pzTUYKiSqgbdbi1QpwirP8OQP",  # Replace with your Google Drive file ID
+            "output_path": "models/doc_summary_model/config.json"
+        },
+        "doc_summary_gc":{
+            "file_id": "134V6b1k0_5QSlEcYTg5hI8Gs-E1rv8Ku",  # Replace with your Google Drive file ID
+            "output_path": "models/doc_summary_model/generation_config.json"
+        },
+        "doc_summary_m":{
+            "file_id": "1Y26GTrEirE0fI5Y61Ss6pmdXpjexbKhs",  # Replace with your Google Drive file ID
+            "output_path": "models/doc_summary_model/model.safetensors"
+        },
+        "doc_summary_tokenizer":{
+            "file_id": "134eW0ylW4WEFLLCRPA3KrnkBmIC1uV6C",  # Replace with your Google Drive file ID
+            "output_path": "models/doc_summary_model/special_tokens_map.json"
+        },
+        "doc_summary_spice":{
+            "file_id": "1W-1jNvlsOCxWe08iyqMUVuNQKqlpkvtd",  # Replace with your Google Drive file ID
+            "output_path": "models/doc_summary_model/spiece.model"
+        },
+        "doc_summary_tc":{
+            "file_id": "1U6pleQC-pXzUVrzfBTeBc0VuBynxscBH",  # Replace with your Google Drive file ID
+            "output_path": "models/doc_summary_model/tokenizer_config.json"
         },
         "vit_model":{
             "file_id": "1SoHRU9vLQeQjVzMm3D5o4GzXdZjdXEqd",  # Replace with your Google Drive file ID
